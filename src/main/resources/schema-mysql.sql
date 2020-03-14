5e1ff03bf8ae3d4f6574b030dfe8d8ad6142ef2c
David Keen <david.keen@keen.org>
2020-03-14 16:51:16 +1100

    Add database creation statement for MySQL

--- src/main/resources/schema-mysql.sql
+++ src/main/resources/schema-mysql.sql
@@ -1,2 +1,4 @@
+CREATE SCHEMA IF NOT EXISTS solar
+USE solar
 CREATE TABLE IF NOT EXISTS current_power (id INTEGER AUTO_INCREMENT, timestamp DATETIME, generation NUMERIC(6,2), consumption NUMERIC(6,2), primary key (id))
 CREATE TABLE IF NOT EXISTS generation_forecast (id INTEGER AUTO_INCREMENT, pv_estimate NUMERIC(6,4), pv_estimate10 NUMERIC(6,4), pv_estimate90 NUMERIC(6,4), period_end DATETIME, period VARCHAR(50), primary key (id))
