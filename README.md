91c7287bd7429149003f79a9a0218b5ca1255446
David Keen <david.keen@gmail.com>
2021-06-19 15:15:00 +1000

    Improve documentation

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # Solar monitoring app
-Monitors solar panel output.
-* Instantaneous power generation and consumption (via Fronius inverter API)
-* Retrieves Solcast solar forecasts
+Monitors solar panel output using the Fronius inverter API and power consumption in realtime and 
+persists the data to a MySQL database for display in Grafana.
+
+* Monitors instantaneous power generation and consumption
+* Monitors per-string power generation
+* Retrieves Solcast solar forecasts (https://solcast.com/)
 * Uploads actual generation to tune Solcast forecast
 
-## Roadmap
-* Add cost of energy
-* Retrieve per-string output
+# Usage
+Configure solar panel system in Solcast
+
+Setup and run the app:
+* Build the application using Maven: `mvn clean package`
+* Install and run MySQL (https://www.mysql.com/ or https://mariadb.org/)
+* Setup the database using `/src/main/resources/mysql/setup.sql` (substitute your own passwords)
+* Configure properties in `application.properties`
+* Run the application
+
+To view the output, use Grafana:
+* Install and run Grafana
+* Configure the MySQL datasource
+* Import the Solar dashboard found in `/src/main/resources/grafana/Solar.json`
---
+++ src/main/resources/mysql/setup.sql
@@ -0,0 +1,12 @@
+-- Create schema and tables for storing data
+CREATE SCHEMA IF NOT EXISTS solar
+USE solar
+CREATE TABLE IF NOT EXISTS current_power (id SERIAL, inverter_timestamp VARCHAR(50), inverter_epoch_timestamp BIGINT, application_timestamp VARCHAR(50), generation NUMERIC(6,2), consumption NUMERIC(6,2), uploaded BOOLEAN, primary key (id), index idx_inverter_epoch_timestamp (inverter_epoch_timestamp))
+CREATE TABLE IF NOT EXISTS generation_forecast (id SERIAL, pv_estimate NUMERIC(6,4), pv_estimate10 NUMERIC(6,4), pv_estimate90 NUMERIC(6,4), period_end VARCHAR(50), period_end_epoch BIGINT, period VARCHAR(50), primary key (id), index idx_period_end_epoch (period_end_epoch))
+CREATE TABLE IF NOT EXISTS string_power (id SERIAL, period_end VARCHAR(50), period_end_epoch BIGINT, period VARCHAR(50), string1_volts NUMERIC(5,2), string1_amps NUMERIC(3,2), string1_power NUMERIC(7,3), string2_volts NUMERIC(5,2), string2_amps NUMERIC(3,2), string2_power NUMERIC(7,3), primary key (id), unique index idx_period_end_epoch (period_end_epoch))
+-- Create application user
+CREATE USER IF NOT EXISTS 'solarapp'@localhost IDENTIFIED BY 'put-your-password-here'
+GRANT SELECT, INSERT, UPDATE ON solar.* TO solarapp@localhost
+-- Create Grafana user
+CREATE USER IF NOT EXISTS 'grafana'@localhost IDENTIFIED BY 'put-your-password-here'
+GRANT SELECT ON solar.* TO grafana@localhost
