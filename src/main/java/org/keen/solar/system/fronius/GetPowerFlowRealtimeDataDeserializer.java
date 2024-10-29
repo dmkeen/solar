9c37683e9d63c2400651d916c13b6d92ff17b2fc
David Keen <david.keen@gmail.com>
2024-10-29 17:52:57 +1100

    Release 3.3.1: Round measurements from the GetPowerFlowRealtimeData API to two decimal places

--- src/main/java/org/keen/solar/system/fronius/GetPowerFlowRealtimeDataDeserializer.java
+++ src/main/java/org/keen/solar/system/fronius/GetPowerFlowRealtimeDataDeserializer.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import org.keen.solar.system.domain.CurrentPower;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.Duration;
 import java.time.OffsetDateTime;
 
@@ -33,11 +35,29 @@ public class GetPowerFlowRealtimeDataDeserializer extends StdDeserializer<Curren
         OffsetDateTime inverterTimestamp = OffsetDateTime.parse(timestampString);
 
         JsonNode siteNode = jsonNode.get("Body").get("Data").get("Site");
-        double powerGeneration = siteNode.get("P_PV").asDouble();
-        double powerConsumption = siteNode.get("P_Load").asDouble();
+        double powerGeneration = roundMeasurement(siteNode.get("P_PV").decimalValue());
+        double powerConsumption = roundMeasurement(siteNode.get("P_Load").decimalValue());
 
         return new CurrentPower(inverterTimestamp.toEpochSecond(), inverterTimestamp.getOffset().getTotalSeconds(),
                 Duration.between(inverterTimestamp, applicationTimestamp).getSeconds(),
                 powerGeneration, powerConsumption, false);
+    }
+
+    /**
+     * Returns the given measurement, rounded to two decimal places.<br/>
+     * <br/>
+     * The Fronius API sometimes returns measurements that are slightly above
+     * or below a particular value, apparently due to the imprecision of
+     * representing a floating point value. For example, it might return
+     * a value for P_PV of 1931.2000000001 or 1931.19999999999. These are best
+     * interpreted as 1931.20.
+     *
+     * @param measurement the measurement returned from the API
+     * @return measurement as a double
+     */
+    private static double roundMeasurement(BigDecimal measurement) {
+        return measurement
+                .setScale(2, RoundingMode.HALF_UP)
+                .doubleValue();
     }
 }
