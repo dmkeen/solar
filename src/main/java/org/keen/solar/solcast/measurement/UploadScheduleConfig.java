5585c9248bbc6a03e987bd1c0b3f8e24c9d56a74
David Keen <david.keen@gmail.com>
2024-11-05 21:18:04 +1100

    Release 3.4.2: Don't upload measurements to Solcast on application startup

--- src/main/java/org/keen/solar/solcast/measurement/UploadScheduleConfig.java
+++ src/main/java/org/keen/solar/solcast/measurement/UploadScheduleConfig.java
@@ -1,10 +1,8 @@
 package org.keen.solar.solcast.measurement;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -16,10 +14,8 @@ public class UploadScheduleConfig {
     private MeasurementUploader uploader;
 
     @Async
-    @EventListener(classes = ApplicationReadyEvent.class)
     @Scheduled(cron = "${app.solcast.measurement-upload-cron}")
     public void uploadAll() {
-        // TODO: Don't upload if we've uploaded recently (configurable)
         uploader.uploadAll();
     }
 }
