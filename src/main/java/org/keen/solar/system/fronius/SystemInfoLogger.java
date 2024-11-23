afba69b81ce667c554721075cc8a6d7e52d255db
David Keen <david.keen@gmail.com>
2024-11-23 17:36:45 +1100

    Prevent execution during tests

--- src/main/java/org/keen/solar/system/fronius/SystemInfoLogger.java
+++ src/main/java/org/keen/solar/system/fronius/SystemInfoLogger.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -21,6 +22,7 @@ import java.time.OffsetDateTime;
  * Logs various information about the PV system, both at application startup
  * and daily at midnight.
  */
+@Profile("!test")
 @Service
 public class SystemInfoLogger {
 
