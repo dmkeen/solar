8af582742543ac2059d02eb23830eb34e916ee20
David Keen <david.keen@gmail.com>
2025-07-13 12:15:09 +1000

    Release 4.1.0: Add toggle for Solcast measurement upload function

--- solar-core/src/main/java/org/keen/solar/solcast/measurement/UploadScheduleConfig.java
+++ solar-core/src/main/java/org/keen/solar/solcast/measurement/UploadScheduleConfig.java
@@ -1,6 +1,7 @@
 package org.keen.solar.solcast.measurement;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Async;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 
 @Profile("!test")
 @Configuration
+@ConditionalOnProperty(name = "app.solcast.measurement-upload.enable", havingValue = "true")
 public class UploadScheduleConfig {
 
     @Autowired
